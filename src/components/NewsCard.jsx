be5d62d89deba3114b8ddfddd178b3f1738d5847
diotte-am <mxamare@gmail.com>
2026-06-28 22:14:23 -0400

    fix

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NewsCard({ item, currentLang = 'en' }) {
   const [copied, setCopied] = useState(false);
